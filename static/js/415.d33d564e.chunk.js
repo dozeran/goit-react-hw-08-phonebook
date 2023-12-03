6c9fe1841aa27c1e052ede4f01515a9b4331d7b9
dozeran <130239890+dozeran@users.noreply.github.com>
2023-12-03 16:27:01 +0000

    Deploying to gh-pages from @ dozeran/goit-react-hw-08-phonebook@5b301ccb495001d56669982a6fd4a3ba4b7a2643 🚀

---
+++ static/js/415.d33d564e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return s}});var r=n(890),o=n(184);function s(){return(0,o.jsxs)("div",{children:[(0,o.jsx)("br",{}),(0,o.jsx)("br",{}),(0,o.jsx)("br",{}),(0,o.jsx)("br",{}),(0,o.jsx)(r.Z,{component:"h1",variant:"h4",children:"Welcome to the best contacts saver"})]})}}}]);
+//# sourceMappingURL=415.d33d564e.chunk.js.map
